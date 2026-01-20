9bfde398a20da410ae0edbdd41850c3592f520b6
kofi arhin <kofiarhin69@gmail.com>
2026-01-20 09:40:15 +0000

    fix userid issue in production with saved product

--- server/models/save.product.model.js
+++ server/models/save.product.model.js
@@ -2,20 +2,21 @@ const mongoose = require("mongoose");
 
 const saveProductSchema = new mongoose.Schema(
   {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+    clerkId: {
+      type: String,
       required: true,
+      index: true,
     },
     productId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Product",
       required: true,
+      index: true,
     },
   },
   { timestamps: true },
 );
 
-saveProductSchema.index({ userId: 1, productId: 1 }, { unique: true });
+saveProductSchema.index({ clerkId: 1, productId: 1 }, { unique: true });
 
 module.exports = mongoose.model("Save", saveProductSchema);
